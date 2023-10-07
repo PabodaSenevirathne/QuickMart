5e267621039b2491348b597b2fcf1fd30e9f0d1f
Paboda Senevirathne <pabodasenevirathne@gmail.com>
2023-10-07 01:35:41 -0400

    validations and calculations updated

--- QuickMart/Program.cs
+++ QuickMart/Program.cs
@@ -10,13 +10,11 @@ namespace QuickMart
 
             List<string> purchasedItemNames = new List<string>();
             List<double> purchasedItemWeights = new List<double>();
-            List<double> purshasedItemPrices = new List<double>();
+            List<double> purchasedItemPrices = new List<double>();
 
             List<string> itemList = new List<string> { "Chillies", "Tomatoes", "Apples", "Milk" };
 
-
-             
-            string loyalityCard = "";
+            string loyaltyCard = "";
             double subTotal = 0.0;
             double itemPrice = 0.0;
             double itemAmount = 0.0;
@@ -27,17 +25,18 @@ namespace QuickMart
             Console.WriteLine("Paboda Jayamali Senevirathne, 8927315, Psenevirathne7315@conestogac.on.ca\n");
 
             Console.WriteLine("=========================================================================\n");
+
             // display available item list
             Console.WriteLine("Available Items: " + string.Join(", ", itemList));
 
             while (true)
             {
-                Console.WriteLine("\nPlease enter the name of the sales item purshased or enter 'done' to finish");
+                Console.WriteLine("\nPlease enter the name of the purchased item or enter 'Done' to finish");
                 string item = Console.ReadLine()?.ToLower() ?? "default";
 
                 if (purchasedItemNames.Contains(item))
                 {
-                    Console.WriteLine($"You have already purchased the {item}.");
+                    Console.WriteLine($"You have already purchased {item}.");
                     Console.WriteLine("Remaining items: " + string.Join(", ", itemList));
                     continue;
                 }
@@ -69,52 +68,58 @@ namespace QuickMart
                 }
                 else
                 {
-                    Console.WriteLine("Please enter a valid item name");
+                    Console.WriteLine("Please enter a valid item!");
                     continue;
                 }
 
-
-                // get item weight according to the lb or carton
-                if (item == "Chillies" || item == "Tomatoes" || item == "Apples")
+                // get item weight based on lb or carton quantity
+                if (item == "chillies" || item == "tomatoes" || item == "apples")
                 {
-                    Console.WriteLine($"How much did the {item}s weigh, in total (in lb):");
+                    Console.WriteLine($"How much did the {item} weigh, in total (in lb):");
                     
                 }
                 else
                 {
-                    Console.WriteLine($"How much did the {item}s weigh, in total (in carton):");
+                    Console.WriteLine($"How much did the {item} weigh, in total (in carton):");
                     
                 }
 
                 //validate weight
                 if (!int.TryParse(Console.ReadLine(), out int itemWeigh) || itemWeigh <= 0)
                 {
-                    Console.WriteLine("Invalid weight. Please enter a valid weight.");
+                    Console.WriteLine("Invalid weight! Please enter a valid weight.");
                     continue;
                 }
 
                 //calculate initial item price
                 itemAmount = itemPrice * itemWeigh;
 
-                //add purshased items to purchasedItemNames list
+                //add purchased items to purchasedItemNames list
                 purchasedItemNames.Add(item);
                 purchasedItemWeights.Add(itemWeigh);
-                purshasedItemPrices.Add(itemPrice);
+                purchasedItemPrices.Add(itemPrice);
 
                 // remove item from the item list
                 itemList.Remove(item);
 
 
-                //get wheather is there a loyality card at once
-                if (loyalityCard == "")
+                //check loyalty card availability (initial question)
+                if (loyaltyCard == "")
                 {
-                    Console.WriteLine($"Do you have a Loyality Card(Yes/No)?:");
-                    loyalityCard = Console.ReadLine()?.ToLower() ?? "default";
+                    Console.WriteLine($"Do you have a Loyalty Card (Yes/No)?");
+                    loyaltyCard = Console.ReadLine()?.ToLower() ?? "default";
+
+                    if (loyaltyCard != "yes" && loyaltyCard != "no")
+                    {
+                        Console.WriteLine("Please enter a valid input (Yes/No)!");
+                        loyaltyCard = Console.ReadLine()?.ToLower() ?? "default";
+                    }
 
                 }
+
+                // check if the item has discount or not before calculating the discounted price
                 bool hasItemDiscount;
 
-                // check if the items has discount or not before calculate discounted price
                 if (item == "chillies" || item == "tomatoes" || item == "apples")
                 {
                     
@@ -125,46 +130,47 @@ namespace QuickMart
                     hasItemDiscount = false;
                 }
 
-              
-             
+                
+
                 //calculate discounted price
-                if (loyalityCard == "yes" && hasItemDiscount)
+                if (loyaltyCard == "yes" && hasItemDiscount)
                 {
                     discountedPrice = itemAmount * 0.1;
                     totalDiscount += discountedPrice;
-
-                    //itemAmount = (itemAmount - discountedPrice);
                 }
 
-
+                subTotal += itemAmount;
             }
     
-           
-            // get used store bag count
-            Console.WriteLine($"How much store bags do you need?");
+            //calulate store bag cost
+            Console.WriteLine($"How many store bags do you need?");
             if (!int.TryParse(Console.ReadLine(), out int storeBagsCount) || storeBagsCount < 0)
+
             {
-                Console.WriteLine("Please enter a valid number.");
+                Console.WriteLine("Please enter a valid number!");
             }
 
-           // storeBagsCount = Convert.ToInt32(Console.ReadLine());
             double storeBagsCost = storeBagsCount * 0.50;
+
             Console.WriteLine("=========================================================================\n");
             Console.WriteLine("---------------------------QUICK MART SHOP-------------------------------\n");
-            Console.WriteLine("-------------------------------Reciept-----------------------------------\n");
+            Console.WriteLine("-------------------------------Receipt-----------------------------------\n");
             Console.WriteLine("=========================================================================\n");
+
+
+            //display receipt
             for (int i = 0; i < purchasedItemNames.Count; i++)
             {
-                Console.WriteLine($"Item: {purchasedItemNames[i]}\nQty: {purchasedItemWeights[i]}\nBase: ${purshasedItemPrices[i]}\n");
+                Console.WriteLine($"Item: {purchasedItemNames[i]}\nQty: {purchasedItemWeights[i]}\nBase: ${purchasedItemPrices[i]}\n");
             }
             
             Console.WriteLine($"Number of Bags: {storeBagsCount}");
             Console.WriteLine($"Price of Bags: {storeBagsCost}");
-            Console.WriteLine($"Loyality Card: {loyalityCard}");
+            Console.WriteLine($"Loyalty Card: {loyaltyCard}");
 
             Console.WriteLine("\n-------------------------------------------------------------------------\n");
-            double grossAmount = itemAmount + storeBagsCost;
-            Console.WriteLine($"SubTotal : {grossAmount}");
+            subTotal = subTotal + storeBagsCost;
+            Console.WriteLine($"Sub Total : {subTotal}");
 
             {
                 Console.WriteLine("--\n");
@@ -172,21 +178,19 @@ namespace QuickMart
                 Console.WriteLine("\n--\n");
             }
 
-            grossAmount = (grossAmount - totalDiscount);
+            subTotal = subTotal - totalDiscount;
 
-            Console.WriteLine($"SubTotal: {grossAmount}");
+            Console.WriteLine($"Sub Total: {subTotal:F2}");
 
-            //calculate subtotal
-
-            tax = grossAmount * 0.13;
-            subTotal = (grossAmount + tax) + subTotal;
+            //calculate total price after tax
+            tax = subTotal * 0.13;
+            subTotal = subTotal + tax;
 
             Console.WriteLine($"Tax: {tax:F2}\n");
 
             Console.WriteLine("========================================================================\n");
             Console.WriteLine($"TOTAL: {subTotal:F2}");
             Console.WriteLine("========================================================================\n");
-
 
         }
     }
